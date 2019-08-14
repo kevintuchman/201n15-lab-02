92540e533adae8d74686a4b793a053cb0bbba196
kevintuchman <kevin.a.tuchman@gmail.com>
2019-08-14 06:48:46 -0700

    js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
 "use strict";
-prompt("what is your name");
+
+alert('I like watching a good show; being stuck in Alaska does this to you. Answer these five questions to know a little more about myself and the shows I enjoy watching.')
+
+var rickMorty = prompt('Have I ever Rick and Morty');
+rickMorty = rickMorty.lowercase();
+console.log('I enjoy a good cartoon that makes me laugh. ' + rickMorty )
