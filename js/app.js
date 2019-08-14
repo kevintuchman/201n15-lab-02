4abc58b44e80a4dd1dec34abd5571ee59b1fe2f2
kevintuchman <kevin.a.tuchman@gmail.com>
2019-08-14 08:26:17 -0700

    css and js

--- js/app.js
+++ js/app.js
@@ -5,9 +5,44 @@ alert('I like watching a good show; being stuck in Alaska does this to you. Answ
 var rickMorty = prompt('Have I ever watched Rick and Morty?');
 //  rickMorty = rickMorty.lowercase();
 
-if(likes rickMorty.toUppercase() === 'YES'){
+if(watch rickMorty.toUppercase() === 'yesS'){
   alert('Correct, but I could care less if they released a new season now.')
-} else{
+} else  {
   alert('Incorrect.');
 }
 
+var boJack = prompt ('Have I ever watched Bojack Horseman?');
+// boJack = boJack.lowercase();
+
+if(watch boJack.toUppercase() === 'yes'){
+  alert('Correct, but I found the last season lackluster.')
+} else  {
+  alert('Incorrect.');
+}
+
+var gameOfthrones = prompt ('Have I ever watched Game of Thrones?');
+// gameOfthrones = gameOfthrones.lowercase();
+
+if(didnotwatch gameOfthrones.toUppercase() === 'no');{
+  alert('Correct, I only seen 15 minutes of it before deciding it was not for me.')
+} else {
+  alert('Incorrect.');
+}
+
+var joJo = prompt ('Have I ever watched Jojo?');
+// joJo = joJo.lowercase();
+
+if(watch joJo.toUppercase() === 'yes'){
+  alert('Correct, I watch a little bit with my dad now when I have the time.')
+} else {
+  alert('Incorrect.')
+}
+
+var samuraiChamploo = prompt ('Have I ever watched Samurai Champloo?');
+// samuraiChamploo = samuraiChamploo.lowercase();
+
+if(watch samuraiChamploo.toUpperCase() === 'yes');{
+  alert('Correct, I watched a few years ago in Kodiak.')
+} else {
+  alert('Incorrect.')
+}
