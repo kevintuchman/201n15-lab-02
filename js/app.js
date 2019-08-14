d6cd9de941de7ea3b9a7e76f011514f4222730ca
kevintuchman <kevin.a.tuchman@gmail.com>
2019-08-14 07:11:46 -0700

    js and css

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@
 
 alert('I like watching a good show; being stuck in Alaska does this to you. Answer these five questions to know a little more about myself and the shows I enjoy watching.')
 
-var rickMorty = prompt('Have I ever Rick and Morty');
-rickMorty = rickMorty.lowercase();
-console.log('I enjoy a good cartoon that makes me laugh. ' + rickMorty )
+var rickMorty = prompt('Have I ever watched Rick and Morty?');
+//  rickMorty = rickMorty.lowercase();
+
+if(likes rickMorty.toUppercase() === 'YES'){
+  alert('Correct, but I could care less if they released a new season now.')
+} else{
+  alert('Incorrect.');
+}
+
