038763a022f135b9523bc87dcfc09e98244e407c
kevintuchman <kevin.a.tuchman@gmail.com>
2019-08-15 19:51:23 -0700

    css and js changes

--- js/app.js
+++ js/app.js
@@ -5,11 +5,7 @@ alert('I like watching a good show; being stuck in Alaska does this to you. Answ
 var rickMorty = prompt('Have I ever watched Rick and Morty?');
 //  rickMorty = rickMorty.lowercase();
 
-<<<<<<< HEAD
-if(rickMorty.toUppercase() === 'yes'){
-=======
-if(watch rickMorty.toUppercase() === 'yesS'){
->>>>>>> 9223bf76e58d093ceb9b394255b232689389cc4b
+if(rickMorty.toUppercase() === 'yesS'){
   alert('Correct, but I could care less if they released a new season now.')
 } else  {
   alert('Incorrect.');
@@ -18,11 +14,7 @@ if(watch rickMorty.toUppercase() === 'yesS'){
 var boJack = prompt ('Have I ever watched Bojack Horseman?');
 // boJack = boJack.lowercase();
 
-<<<<<<< HEAD
 if(boJack.toUppercase() === 'yes'){
-=======
-if(watch boJack.toUppercase() === 'yes'){
->>>>>>> 9223bf76e58d093ceb9b394255b232689389cc4b
   alert('Correct, but I found the last season lackluster.')
 } else  {
   alert('Incorrect.');
@@ -31,13 +23,8 @@ if(watch boJack.toUppercase() === 'yes'){
 var gameOfthrones = prompt ('Have I ever watched Game of Thrones?');
 // gameOfthrones = gameOfthrones.lowercase();
 
-<<<<<<< HEAD
 if(gameOfthrones.toUppercase() === 'no'){
-  alert('Correct, I only seen 15 minutes of it before deciding it was not for me.');
-=======
-if(didnotwatch gameOfthrones.toUppercase() === 'no');{
   alert('Correct, I only seen 15 minutes of it before deciding it was not for me.')
->>>>>>> 9223bf76e58d093ceb9b394255b232689389cc4b
 } else {
   alert('Incorrect.');
 }
@@ -45,29 +32,17 @@ if(didnotwatch gameOfthrones.toUppercase() === 'no');{
 var joJo = prompt ('Have I ever watched Jojo?');
 // joJo = joJo.lowercase();
 
-<<<<<<< HEAD
 if(joJo.toUppercase() === 'yes'){
-=======
-if(watch joJo.toUppercase() === 'yes'){
->>>>>>> 9223bf76e58d093ceb9b394255b232689389cc4b
-  alert('Correct, I watch a little bit with my dad now when I have the time.')
+  alert('Correct, I watch a little bit with my dad now when I have the time.');
 } else {
-  alert('Incorrect.')
+  alert('Incorrect.');
 }
 
 var samuraiChamploo = prompt ('Have I ever watched Samurai Champloo?');
 // samuraiChamploo = samuraiChamploo.lowercase();
 
-<<<<<<< HEAD
 if(samuraiChamploo.toUpperCase() === 'yes'){
   alert('Correct, I watched a few years ago in Kodiak.');
 } else {
   alert('Incorrect.');
 }
-=======
-if(watch samuraiChamploo.toUpperCase() === 'yes');{
-  alert('Correct, I watched a few years ago in Kodiak.')
-} else {
-  alert('Incorrect.')
-}
->>>>>>> 9223bf76e58d093ceb9b394255b232689389cc4b
