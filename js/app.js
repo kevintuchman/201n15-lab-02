a75fbd9396fa73917fba83f0fe939f5481b1a794
kevintuchman <kevin.a.tuchman@gmail.com>
2019-08-22 20:42:01 -0700

    question seven

--- js/app.js
+++ js/app.js
@@ -66,7 +66,7 @@ tvShow();
 
 function sixthQuestion(totalStates){
   var i = 0
-  while (i < 5){
+  while (i < 4){
     i++;
    
     var countStates = prompt(userName + ", how many states have I lived in?");
@@ -77,16 +77,51 @@ function sixthQuestion(totalStates){
         break;
       } else if (countStates <= 1) {
         alert('That\'s too low ' + userName + '.');
-        if(i < 5){alert ('That was Attempt Number ' + i +'/5');}
+        if(i < 4){alert ('That was Attempt Number ' + i +'/4');}
       } else if (countStates >= totalStates) {
         alert('I don\'t move around that much ' + userName + '.');
-        if(i < 5){alert ('That was Attempt Number ' + i +'/5');}
+        if(i < 4){alert ('That was Attempt Number ' + i +'/4');}
       }
       else if (typeof (countStates) === 'string')  {
-        alert('Value has to be integer to work.');
+        alert('Value has to be an integer to work.');
       // }
       }
     }
   }
 
 sixthQuestion(2);
+
+function seventhQuestion(){
+  for (var j = 1; j <= 6; j++){
+    var pokémonNames = ["Gyarados", "Raichu", "Alolan Raichu", "Beedrill", "Mega Beedrill", "Ralts", "Espeon", "Gardevoir", "Flygon", "Metagross", "Trapinch", "Cyndaquil", "Totodile", "Typhlosion", "Feraligatr", "Primarina", "Incineroar", "Nidoking", "Salamence", "Sawsbuck", "Chandelure", "Haxorus", "Aegislash", "Mega Mawile", "Noivern", "Tapu Koko", "Tapu Lele", "Tapu Fini", "Tapu Bulu", "Blacephalon", "Bisharp", "Joltik", "Galvantula", "Volcarona", "Larvesta", "Garchomp", "Staraptor", "Empoleon", "Mega Manectric", "Dragonite", "Alolan Marowak", "Arcanine", "Regice", "Alolan Ninetails", "Ninetails", "Torkoal", "Venusaur", "Bulbasaur", "Squirtle", "Blastoise", "Hawlucha", "Froslass", "Zekrom", "Mimikyu", "Azumarill", "Marill", "Mega Lopunny", "Lucario", "Mega Lucario", "Exploud", "Toxapex", "Loudred", "Whismur", "Silvally", "Naganadel", "Pheromosa", "Xurkitree", "Rotom"]
+    
+    for (var i = 0; i < pokémonNames.length; i++){
+      console.log(pokemonNames[i] + " " + " is a Pokémon name");
+    }
+    alert('Attempt Number '+j+'/6');
+    var guessPokemon = prompt("Name a Pokémon I like!")
+
+    console.log("Guess Pokémon" + " " + guessPokemon)
+      guessPokemon = guessPokemon.toLowerCase();
+
+    if (pokemonNames.indexOf(guessPokemon) > -1)  {
+      alert(guessPokemon + " is one of my favorites!")
+      count++;
+      break;
+    } else {
+      alert(guessPokemon + " isn\t one of my favorites.")
+    }
+    }
+
+    alert(userName + ' your total score is ' + count + '/7');
+
+  }
+  
+  seventhQuestion();
+
+  if (count >= 5) {
+    alert(userName + ' didn\'t fail!');
+  }
+  else {
+    alert(userName + ' will try harder next time!');
+  }
